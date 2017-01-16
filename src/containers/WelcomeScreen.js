8d79de5d1cd79f090848ef2533ac2247ce2f7693
Natalia <natalia@Natalias-MacBook-Pro.local>
2017-01-16 11:08:50 +0000

    adds main onboarding components

---
+++ src/containers/WelcomeScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Button from 'apsl-react-native-button';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+
+export class Welcome extends Component {
+  render () {
+    return (
+      <View>
+        <Text>Welcome</Text>
+      </View>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import {LoginScreen} from './containers';
+import {WelcomeScreen} from './containers';
+
+export class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      user: null
+    };
+  }
+  renderComponent (Component) {
+    return (
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
+  }
+  render () {
+    return this.state.user === null ? this.renderComponent(<LoginScreen />) : this.renderComponent(<WelcomeScreen />);
+  }
+}
